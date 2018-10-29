4fd740b73e2c6341ece4bf761d7dacea38564afa
Ray Lomeli <raylom96@gmail.com>
2018-10-29 14:36:01 -0500

    added index.js file

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const helmet = require("helmet");
+
+const notessRoutes = require("./notes/notesRoutes.js");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+// sanity check endpoint
+server.get("/", (req, res) => {
+  res.send("It's Alive");
+});
+
+server.use("/api/courses", notesRoutes);
+
+server.listen(9000, () => console.log("\nAPI running on 9k\n"));
